3b12ce5d502304f74a5ab037d57d01b435f16826
Eugeniya <bruyko.e@gmai.com>
2023-09-25 20:13:34 -0600

    Добавили проект

---
+++ TestV2App/TestV2App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestV2App
+//
+//  Created by brubru on 25.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
